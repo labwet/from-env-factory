a2384f14bf15fdcce440456d5c283038989ca910
youfoundron <rongierlach@gmail.com>
2025-01-03 09:50:00 -0800

    allow generic types

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@ export function fromEnvFactory<T extends string>(
   factoryFallbackValue?: T
 ) {
   return (invokedFallbackValue = factoryFallbackValue) => {
-    const value = process.env[key];
-    const envVarIsUnset = value === undefined;
-    if (envVarIsUnset && invokedFallbackValue === undefined) {
-      throw new Error(`${key} env var unset with no fallback`);
-    } else if (envVarIsUnset && invokedFallbackValue !== undefined) {
+    const value = process.env[key] as T | undefined;
+    if (value !== undefined) {
+      return value;
+    } else if (invokedFallbackValue !== undefined) {
       return invokedFallbackValue;
     } else {
-      return value!;
+      throw new Error(`${key} env var unset with no fallback`);
     }
   };
 }
